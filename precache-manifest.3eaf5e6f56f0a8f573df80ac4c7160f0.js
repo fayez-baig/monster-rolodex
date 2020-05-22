d2064eb9faaa2dd9026759291f247b6c7d2f7515
fayez baig <coderninja16@gmail.com>
2020-05-22 15:51:34 +0530

    Updates

---
+++ precache-manifest.3eaf5e6f56f0a8f573df80ac4c7160f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade86c1ec2e09e3a262e29149e87fb06",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "36343bb7a4c376324ce2",
+    "url": "/monster-rolodex/static/css/main.a9bad9cc.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36343bb7a4c376324ce2",
+    "url": "/monster-rolodex/static/js/main.c49ecb50.chunk.js"
+  },
+  {
+    "revision": "b46281a00ff77db46658",
+    "url": "/monster-rolodex/static/js/runtime-main.c3b95803.js"
+  }
+]);
